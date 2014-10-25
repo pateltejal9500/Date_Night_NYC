e11708ec0ab4ae238addce21db20a7665b9308dd
pateltejal9500 <pateltejal9500@gmail.com>
2014-10-25 12:42:17 -0400

    dates button

--- datenight/app/controllers/users_controller.rb
+++ datenight/app/controllers/users_controller.rb
@@ -10,12 +10,12 @@ class UsersController < ApplicationController
 
 		User.create(users)
 
-		render :index
+		redirect_to "/login"
 	end
 
 	def show
 		@user = User.find_by(id: session[:user_id])
-		if @user
+		if @user && params[:id] == session[:user_id]
 			respond_to do |format|
 			format.json {render :json => @user, :include =>:plans}
 			format.html {render :index}
